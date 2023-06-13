13bdf1f3a893190654afee000ae8756d348d1acc
Janos Vajda <vajdajanos@gmail.com>
2023-06-13 03:00:01 +0100

    Script should show a message if there is non commited changes.

--- client/publish-client.sh
+++ client/publish-client.sh
@@ -31,29 +31,23 @@ write_commands() {
     echo
 }
 
-# Confirm that all changes have been committed and pushed
-# Set text color to green
-tput setaf 2
-echo "Before you run this script everything must be committed and pushed to git."
-read -p "There should not be any changes in code. Is everything nice and clean? (y/n) " answer
-# Reset text color
-tput sgr0
-
-case ${answer:0:1} in
-    y|Y )
-        echo "Continuing with the script..."
-        # Check if there are uncommitted changes
-        if [[ -z $(git status --porcelain) ]]; then
-            # No changes
-            echo "Code base is clean, no uncommitted changes found."
-        else
-            # Changes
-            tput setaf 1
-            echo "Sorry, but your branch contains uncommitted code. Exiting..."
-            tput sgr0
-            exit 1
-        fi
-esac
+# Check if there are uncommitted changes
+if [[ -z $(git status --porcelain) ]]; then
+    # No changes
+    echo "Code base is clean, no uncommitted changes found."
+else
+    # Changes
+    tput setaf 1
+    echo "Sorry, but your branch contains uncommitted code. Before proceeding, you need to commit all changes."
+    echo "Publishing a new version without committing ensures that the version control system accurately reflects the state of the code for that particular version."
+    echo "It also helps to maintain the integrity of your codebase and avoids confusion or mistakes in future."
+    tput setaf 2
+    echo "You should run 'git status' to see uncommitted files."
+    tput setaf 1
+    echo "Please commit your changes and try again. Exiting..."
+    tput sgr0
+    exit 1
+fi
 
 # Checkout to the main branch and pull the latest changes
 echo "I check out the main branch..."
